a528e94097d7e892ee5740e2b730cc2742e1fe72
Comrade X <106185020+comrade09@users.noreply.github.com>
2026-09-03 13:02:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,7 +18,7 @@ jishudeveloper = madflixofficials
 file_auto_delete = humanize.naturaldelta(jishudeveloper)
 
 WLCM = """
-Hey! {first} I am Lexica <a href=https://i.ibb.co/vCJrx4PN/x.jpg >.</a>
+Hey! {first} I am Lexica <a href=https://i.ibb.co/7J4pNCzF/file-61.jpg >.</a>
 """
 
 
