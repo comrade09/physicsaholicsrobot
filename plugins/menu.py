4af0b0eaa3db4953df2dce60807a303488caa553
Comrade X <106185020+comrade09@users.noreply.github.com>
2026-09-08 15:13:09 +0530

    Update menu.py

--- plugins/menu.py
+++ plugins/menu.py
@@ -24,7 +24,8 @@ async def hlpcallback(client: Bot, query: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton(text="🌀 Module Solution", callback_data="module_solution", style=ButtonStyle.PRIMARY),
-                        InlineKeyboardButton(text="🌀 Batch", callback_data="lectures", style=ButtonStyle.PRIMARY),
+                        InlineKeyboardButton(text="🌀 Batches", callback_data="lectures", style=ButtonStyle.PRIMARY),
+                        InlineKeyboardButton(text="🌀 Games", callback_data="games", style=ButtonStyle.PRIMARY),
                     ],
                     [
                         InlineKeyboardButton(text="🔙 Back", callback_data="start", style=ButtonStyle.PRIMARY),
