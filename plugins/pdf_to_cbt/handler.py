83c093250963c01a125ee0d0a088a468061d2501
Comrade X <106185020+comrade09@users.noreply.github.com>
2026-09-19 22:19:54 +0530

    Update handler.py

--- plugins/pdf_to_cbt/handler.py
+++ plugins/pdf_to_cbt/handler.py
@@ -34,8 +34,8 @@ from pyrogram.types import (
     Message,
 )
 
-from . import session
-from .gemini_client import GeminiExtractionError, convert_pdfs_to_cbt_html
+import session
+from pdf_to_cbt.gemini_client import GeminiExtractionError, convert_pdfs_to_cbt_html
 
 logger = logging.getLogger(__name__)
 
